8fb77daf2ffc152c3ffd3bffe99342c4b26dd4c6
azxx <996800442@qq.com>
2022-10-14 18:50:15 +0800

    配置swagger，实现获取题目列表的接口，并且处理分页。 目前配置： 数据库-user,problem,submit,category gin-配置跑通 gorm-配置连接mysql跑通 swagger-配置成功，能够让项目成功运行

--- myOj/router/app.go
+++ myOj/router/app.go
@@ -2,15 +2,23 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"myOj/service"
 )
+
+import _ "github.com/swaggo/gin-swagger" // gin-swagger middleware
+import _ "github.com/swaggo/files"       // swagger embed files
+import _ "myOj/docs"
 
 func Router() *gin.Engine {
 	r := gin.Default()
 
+	// Swagger配置
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	// 做些操作,路由规则
 	r.GET("/ping", service.Ping)
-	r.GET("/problem-list", service.GerProblemList)
+	r.GET("/problem-list", service.GetProblemList)
 
 	return r
 }
--- myOj/service/problem.go
+++ myOj/service/problem.go
@@ -1,12 +1,52 @@
 package service
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
+	"myOj/define"
 	"myOj/models"
 	"net/http"
+	"strconv"
 )
 
-func GerProblemList(c *gin.Context) {
-	models.GetProblemList()
-	c.String(http.StatusOK, "Get Problem List")
+// GetProblemList
+// @Tags 公共方法
+// @Summary 问题列表
+// @Param page query int false "请输入当前页，默认第一页"
+// @Param size query int false "size"
+// @Param keyword query string false "查询关键词"
+// @Success 200 {string} json "{"code":"200","data":""}"
+// @Router /problem-list [get]
+func GetProblemList(c *gin.Context) {
+	size, err := strconv.Atoi(c.DefaultQuery("size", define.DefaultSize))
+	if err != nil {
+		fmt.Println("Get ProblemList Size strconv err ", err)
+	}
+
+	// page是1，其实是从0开始的offset
+	page, err := strconv.Atoi(c.DefaultQuery("page", define.DefaultPage)) // 设置默认值
+	if err != nil {
+		fmt.Println("Get ProblemList Page strconv err ", err)
+	}
+	page = (page - 1) * size // 起始位置
+	var count int64
+	keyword := c.Query("keyword")
+
+	tx := models.GetProblemList(keyword) // 拿到了查到的DB
+
+	list := make([]*models.Problem, 0)
+	err = tx.Count(&count).Offset(page).Limit(size).Find(&list).Error
+	if err != nil {
+		log.Println("Get ProblemList err ", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": map[string]interface{}{
+			"list":  list,
+			"count": count,
+		},
+	})
 }
